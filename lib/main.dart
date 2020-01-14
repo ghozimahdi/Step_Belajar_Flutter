35e6b1ceefe0f8ae88b4946714d32ab8e9a275b6
ghozimahdi <profghozimahdi@gmail.com>
2020-01-14 11:13:23 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(
+            Icons.adb,
+            color: Colors.white,
+          ),
+          title: Text(
+            "AppBar Example",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.settings), onPressed: () {}),
+            IconButton(icon: Icon(Icons.exit_to_app), onPressed: () {}),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: FractionalOffset.topLeft,
+                    end: FractionalOffset.bottomRight,
+                    colors: [Color(0xff0096ff), Color(0xff6610f2)]),
+                image: DecorationImage(
+                  image: AssetImage(
+                      "assets/images/pattern.jpg"),
+                  fit: BoxFit.none,
+                  repeat: ImageRepeat.repeat,
+                )),
+          ),
+        ),
+      ),
+    )
+    ,
+    );
+  }
+}
